2e7d2538b7d410b080142392ddfe3483e49e4176
Katherine Erickson <katherine.erickson@gmail.com>
2016-07-20 18:28:11 -0400

    Remove in-progress matrix.py draft

--- matrix.py
+++
@@ -1,8 +0,0 @@
-
-
-    def exit(self):
-        # Reinitialize all rows and columns as input at exit
-        for i in range(len(self.ROW)):
-                GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        for j in range(len(self.COLUMN)):
-                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
